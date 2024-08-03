b1f1fa45ce5532779ccb69f3a566b1122fc2d436
shreyash <shreyashsonkusare87@gmail.com>
2024-08-03 15:38:08 +0530

    created first version of movie app

---
+++ frontend/src/components/skeletons/WatchPageSkeleton.jsx
@@ -0,0 +1,13 @@
+const WatchPageSkeleton = () => {
+  return (
+    <div className="animate-pulse">
+      <div className="bg-gray-700 rounded-md w-40 h-6 mb-4 shimmer"></div>
+      <div className="bg-gray-700 rounded-md w-40 h-6 mb-4 shimmer"></div>
+      <div className="bg-gray-700 rounded-md w-40 h-6 mb-4 shimmer"></div>
+      <div className="bg-gray-700 rounded-md w-40 h-6 mb-4 shimmer"></div>
+      <div className="bg-gray-700 rounded-md w-40 h-6 mb-4 shimmer"></div>
+    </div>
+  );
+};
+
+export default WatchPageSkeleton;
